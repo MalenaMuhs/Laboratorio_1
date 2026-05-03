1c41f097b2d5ba78ed4b744668b8fc47ae14ee8d
MalenaM. <146149142+MalenaMuhs@users.noreply.github.com>
2026-05-03 17:52:56 -0300

    actualizacion del codigo

--- src/main/java/Empresa_MEG/Test.java
+++ src/main/java/Empresa_MEG/Test.java
@@ -4,6 +4,7 @@ Alumnos: Nicolas Arumay y Malena Muhs
 package Empresa_MEG;
 import java.util.Date;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
@@ -11,13 +12,77 @@ public class Test {
             int mes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el mes: "));
             int año = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su año: "));
             LocalDate fecha = LocalDate.of(año, mes, dia);*/
-            Date hoy = new Date();
 
-        Empleado e1 = new operario(123, "Nicolas", "Arumay", "1100327885", "Calle 123", 
-                                   "example@gmail.com", hoy, 1, "hola", "chau");
-                               
+    
+        int cantOperarios=10;
+        int cantMarketing= 2;
+        int cantVendedores=3;
+        int j = 0;
+       
+        Scanner sc_string = new Scanner(System.in);
+        Scanner sc_int = new Scanner(System.in);
         
-        System.out.println(e1);
+        Empleado[] empleados = new Empleado[15];
+        
+        System.out.println("OPERARIOS");
+        for (int i = 0; i < cantOperarios; i++) {
+            System.out.println("Operario nro. " + i);
+            System.out.print("Nombre: ");
+            String nombre = sc_string.nextLine();
+            System.out.print("Apellido: ");
+            String apellido = sc_string.nextLine();
+            System.out.print("Legajo: ");
+            int legajo = sc_int.nextInt();
+            System.out.print("Telelfono: ");
+            String telefono = sc_string.nextLine();
+            System.out.print("Mail: ");
+            String mail = sc_string.nextLine();
+            System.out.print("Direccion: ");
+            String direccion = sc_string.nextLine();
+            System.out.print("Fecha de nacimiento: ");
+            //nico
+            System.out.print("Horas trabajadas: ");
+            int horas = sc_int.nextInt();
+            System.out.print("Puesto: ");
+            String puesto = sc_string.nextLine();
+            System.out.print("Area: ");
+            String area = sc_string.nextLine();
+            
+            empleados[j]= new operario(legajo,  nombre,  apellido,  telefono,  direccion, mail, new Date() ,  horas,  puesto, area);
+            j++;
+        }
+    
+            System.out.println("VENDEDORES: ");
+        for (int i = 0; i < cantOperarios; i++) {
+            System.out.println("Vendedor nro. " + i);
+            System.out.print("Nombre: ");
+            String nombre = sc_string.nextLine();
+            System.out.print("Apellido: ");
+            String apellido = sc_string.nextLine();
+            System.out.print("Legajo: ");
+            int legajo = sc_int.nextInt();
+            System.out.print("Telelfono: ");
+            String telefono = sc_string.nextLine();
+            System.out.print("Mail: ");
+            String mail = sc_string.nextLine();
+            System.out.print("Direccion: ");
+            String direccion = sc_string.nextLine();
+            System.out.print("Fecha de nacimiento: ");
+            //nico
+            System.out.print("Medio de ventas: ");
+            String medioDeVentas = sc_string.nextLine();
+            System.out.print("Total de ventas en el mes: ");
+            double totalMes = sc_int.nextDouble();
+            
+             empleados[j]= new Vendedor( legajo,  nombre,  apellido,  telefono,  direccion,  mail, new Date(),  medioDeVentas, totalMes);
+       
+            j++;                
+        }
+        
+        
+            
+ 
+                           
     }
  
 }
